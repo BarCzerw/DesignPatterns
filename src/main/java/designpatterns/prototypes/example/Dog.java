44a7827d8968e0a98986390203f40d6d3732ce6c
Bartłomiej <czerwonkabartlomiej@gmail.com>
2021-03-02 20:04:13 +0100

    Prototype - Dog cloned and copied

---
+++ src/main/java/designpatterns/prototypes/example/Dog.java
@@ -0,0 +1,46 @@
+package designpatterns.prototypes.example;
+
+public class Dog implements Cloneable {
+
+    private static int COUNTER = 0;
+
+    private String name;
+    private int age;
+    private int id;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = COUNTER++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public Dog clone() throws CloneNotSupportedException {
+        return (Dog) super.clone();
+    }
+
+    public Dog copy() {
+        return new Dog(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", id=" + id +
+                '}';
+    }
+}
